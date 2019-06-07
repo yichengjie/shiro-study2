3a750aeed0c4b0eabf4b5799a9cb55be30cedac5
yichengjie <626659321@qq.com>
2019-06-07 23:14:11 +0800

    登录测试

---
+++ shiro-demo02/front-end/js/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yicj on 2019/6/7.
+ */
+$(function(){
+    $("#user_view_btn").click(function(){
+        console.info("查看用户信息按钮被点击!")
+        var serverUrl = "/apis/user/view" ;
+        var ajaxing = httpUtil.dealAjaxRequestWithoutParam(serverUrl) ;
+        $.when(ajaxing).done(function(resp){
+            console.info(resp) ;
+        }) ;
+    }) ;
+
+    $("#user_create_btn").click(function(){
+        console.info("创建用户信息按钮被点击!")
+        var serverUrl = "/apis/user/create" ;
+        var ajaxing = httpUtil.dealAjaxRequestWithoutParam(serverUrl) ;
+        $.when(ajaxing).done(function(resp){
+            console.info(resp) ;
+        }) ;
+    }) ;
+}) ;
---
+++ shiro-demo02/front-end/js/login.js
@@ -0,0 +1,18 @@
+/**
+ * Created by yicj on 2019/6/7.
+ */
+$(function(){
+    $("#login-btn").click(function(e){
+        console.info("µÇÂ¼°´Å¥±»µã»÷....")
+        e.preventDefault() ;
+        var username = $("#username").val() ;
+        var password = $("#password").val() ;
+        var formData = {username:username,password:password} ;
+        console.info(formData)
+        var serverUrl = "/apis/ajaxLogin" ;
+        var ajaxing = httpUtil.dealAjaxRequest4JSObj(serverUrl,formData) ;
+        $.when(ajaxing).done(function(resp){
+            console.info(resp)
+        }) ;
+    }) ;
+}) ;
